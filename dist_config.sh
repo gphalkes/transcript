77f3ffb8fe56b4d3cc7fcdab1eea3cf116632c33
Gertjan Halkes <git@ghalkes.nl>
2013-03-24 16:45:07 +0100

    Update the copyright messages; remove unnecessary files

--- dist_config.sh
+++
@@ -1,5 +0,0 @@
-PACKAGE=libtranscript
-SRCDIRS="src `hg manifest | egrep -o 'src\.util/[^/]+' | egrep -v 'Makefile$' | sort | uniq`"
-EXCLUDESRC="/(Makefile|TODO.*|SciTE.*|run\.sh|test\.c|aliases_full\.txt)$"
-GENSOURCES="`echo src/generic_fallbacks.{c,h} src/tables/*.c src.util/ucm2ltc/.objects/*.{cc,c,h}`"
-VERSIONINFO="1:0:0"
